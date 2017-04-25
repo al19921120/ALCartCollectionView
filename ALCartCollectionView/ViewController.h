e7cf778294b13a68f7e766e4c9b51d1228bc21c4
al19921120 <hwt@hwt-air.local>
2017-04-25 16:16:37 +0800

    Initial Commit

---
+++ ALCartCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALCartCollectionView
+//
+//  Created by hwt on 17/4/25.
+//  Copyright © 2017年 hwt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ALCartCollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ALCartCollectionView
+//
+//  Created by hwt on 17/4/25.
+//  Copyright © 2017年 hwt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
